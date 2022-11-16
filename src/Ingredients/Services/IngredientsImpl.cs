7484b769e4469655fc1f8bdce52ce1bf15050edd
Mark Rendle <mark.rendle@newday.co.uk>
2022-11-16 12:02:40 +0100

    IngredientsImpl

--- src/Ingredients/Services/IngredientsImpl.cs
+++ src/Ingredients/Services/IngredientsImpl.cs
@@ -1,4 +1,6 @@
-﻿using Ingredients.Data;
+﻿using Grpc.Core;
+using Ingredients.Data;
+using Ingredients.Protos;
 
 namespace Ingredients.Services;
 
@@ -11,5 +13,34 @@ public class IngredientsImpl : Protos.IngredientsService.IngredientsServiceBase
     {
         _toppingData = toppingData;
         _logger = logger;
+    }
+
+    public override async Task<GetToppingsResponse> GetToppings(GetToppingsRequest request, ServerCallContext context)
+    {
+        try
+        {
+            var toppings = await _toppingData.GetAsync(context.CancellationToken);
+
+            var response = new GetToppingsResponse
+            {
+                Toppings =
+                {
+                    toppings.OrderBy(t => t.Id)
+                        .Select(t => new Topping
+                        {
+                            Id = t.Id,
+                            Name = t.Name,
+                            Price = t.Price,
+                        })
+                }
+            };
+
+            return response;
+        }
+        catch (OperationCanceledException ex)
+        {
+            _logger.LogWarning(ex, "GetToppings cancelled");
+            throw;
+        }
     }
 }
