396b2bc82e6531dce8ada4210cf3e051a4880419
Mark Rendle <mark.rendle@newday.co.uk>
2022-11-16 09:45:37 +0100

    Initial commit

---
+++ src/Ingredients.Data/Constants.cs
@@ -0,0 +1,6 @@
+namespace Ingredients.Data;
+
+public static class Constants
+{
+    public const string StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=grpcworkshop;AccountKey=16rOLzu8/EjQFKd5Mcjve+ruUBP2nF1ZWP7khg5NfwEbfhqmPD+te74BrU+3HsTEqaQ6WqckVFxH+AStjTkw+g==;EndpointSuffix=core.windows.net";
+}
---
+++ src/Orders.PubSub/Constants.cs
@@ -0,0 +1,6 @@
+﻿namespace Orders.PubSub;
+
+internal static class Constants
+{
+    public const string ConnectionString = "grpcworkshop.redis.cache.windows.net:6380,password=zbaXLhBylOsETfBx3QmgQZhR9g9FXaXvZAzCaADpkcM=,ssl=True,abortConnect=False";
+}
