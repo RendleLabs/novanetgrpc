1331bf519e440616573e7b78aee96efb104baf4c
Mark Rendle <mark.rendle@newday.co.uk>
2022-11-16 11:31:24 +0100

    Ingredients

---
+++ src/Ingredients/Program.cs
@@ -0,0 +1,32 @@
+using Ingredients.Data;
+using Ingredients.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+if (OperatingSystem.IsMacOS())
+{
+    builder.WebHost.ConfigureKestrel(k =>
+    {
+        k.ListenLocalhost(5002, l =>
+        {
+            l.Protocols = HttpProtocols.Http2;
+        });
+    });
+}
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+builder.Services.AddSingleton<IToppingData, ToppingData>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<IngredientsImpl>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
---
+++ src/Ingredients/Services/IngredientsImpl.cs
@@ -0,0 +1,15 @@
+﻿using Ingredients.Data;
+
+namespace Ingredients.Services;
+
+public class IngredientsImpl : Protos.IngredientsService.IngredientsServiceBase
+{
+    private readonly IToppingData _toppingData;
+    private readonly ILogger<IngredientsImpl> _logger;
+
+    public IngredientsImpl(IToppingData toppingData, ILogger<IngredientsImpl> logger)
+    {
+        _toppingData = toppingData;
+        _logger = logger;
+    }
+}
